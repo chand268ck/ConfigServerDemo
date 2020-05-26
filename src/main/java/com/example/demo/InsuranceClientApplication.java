319a6a9925f636ed30ff22e81469fa7e1b0895d8
chand268ck <chand268ck@gmail.com>
2020-05-26 18:27:22 +0530

    my first commit

---
+++ src/main/java/com/example/demo/InsuranceClientApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class InsuranceClientApplication {
+
+	@Autowired
+	private RestTemplate templete;
+	
+	@Value("${insurance.provider.url}")
+	private String url;
+	
+	@GetMapping("/getPlan")
+	public List<String> getPlans(){
+		 List<String> plans= templete.getForObject(url, List.class);
+		 return plans;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceClientApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate templete() {
+		return new RestTemplate();
+	}
+}
